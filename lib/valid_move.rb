26a6cc8d708c2531a56593f3ee82917073d237cc
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-13 17:16:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  !position_taken?(board, index) || nil
+  return nil if board[index].empty?
+  !position_taken?(board, index) 
   
 end
 
